b4aaba006c220c30f0050f3f3e0a56bdabfc9b72
th3lias <emindlberger@gmail.com>
2024-04-21 18:42:06 +0200

    whatever :bug:

--- grid/grid_provider.py
+++ grid/grid_provider.py
@@ -1,12 +1,20 @@
 """
 Provides sparse grids and random grids.
 """
-import numpy
 import numpy as np
-
 from grid.grid_type import GridType
 
 
 class GridProvider:
-    def __init__(self):
+    def __init__(self,
+                 dimension: np.int8,
+                 upper_bound: np.ndarray,
+                 lower_bound: np.ndarray,
+                 seed: np.int8 = None):
+        self.dim = dimension
+        self.upper_bound = upper_bound
+        self.lower_bound = lower_bound
+        self.seed = seed
+
+    def generate(self, grid_type: GridType) -> np.ndarray:
         pass
