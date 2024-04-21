341fdfce264d2bbb99c9f0b114719d298ed82751
th3lias <emindlberger@gmail.com>
2024-04-21 17:50:50 +0200

    rework class structure :fire:

--- grid/grid_provider.py
+++ grid/grid_provider.py
@@ -8,29 +8,5 @@ from grid.grid_type import GridType
 
 
 class GridProvider:
-
-    def __init__(self,
-                 grid_type: GridType,
-                 dimension: np.int8,
-                 upper: np.ndarray,
-                 lower: np.ndarray,
-                 seed: np.int8 = None):
-        if not isinstance(grid_type, GridType):
-            raise TypeError("Invalid grid type")
-        self.grid_type = grid_type
-
-    def generate(self) -> numpy.ndarray:
-        """
-        Generates a grid according to type and returns it.
-        :return:
-        """
-        if self.grid_type == GridType.REGULAR:
-            return self._generate_regular()
-        else:
-            return self._generate_random()
-
-    def _generate_regular(self):
-        raise NotImplementedError
-
-    def _generate_random(self):
-        raise NotImplementedError
+    def __init__(self):
+        pass
