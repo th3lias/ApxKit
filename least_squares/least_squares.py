bbcedd8ea0ead19e56d7af101b0704acfc379c20
Jakob Eggl <jakob.eggl@gmail.com>
2024-04-24 07:57:41 +0200

    Tried the LSMR algorithm. Makes it very memory efficient, but also takes a long time to compute.

--- least_squares/least_squares.py
+++ least_squares/least_squares.py
@@ -5,6 +5,8 @@ from sklearn.linear_model import LinearRegression
 from typing import Callable, Union
 from genz.genz_functions import get_genz_function
 from genz.genz_functions import GenzFunctionType
+from utils.utils import ell_2_error_estimate
+from scipy.sparse.linalg import lsmr
 
 
 def approximate_by_polynomial_with_least_squares(f: Callable, dim: np.int8, degree: Union[np.int8, None],
@@ -23,37 +25,58 @@ def approximate_by_polynomial_with_least_squares(f: Callable, dim: np.int8, degr
 
     X_poly = poly.fit_transform(grid)
 
-    model = LinearRegression()
-    model.fit(X_poly, y)
+    res = lsmr(X_poly, y)
 
-    return model
+    coef = res[0]
+    n_iter = res[2]
+
+    print(f'Number of iterations: {n_iter}')
+    print(coef) # returns combin(d + n, n) coefficients (without intercept)
+
+    # model = LinearRegression()
+    # model.fit(X_poly, y)
+
+    # return model
+
+    return coef
+
+
 
 
 if __name__ == '__main__':
     # TODO: Does not work well (but maybe functional -> not sure for now) TODO: Basis of multivariate polynomial
     #  function grows with combin(d+n, d) where d is the dimension and n the number of variables
     np.random.seed(23)
-    n_samples = 25
+    n_samples = 1000
     n_test_samples = 10
 
-    d = np.int8(10)
+    dimension = np.int8(10)
 
-    c = np.random.uniform(size=d)
-    w = np.random.uniform(size=d)
+    c = np.random.uniform(size=dimension)
+    w = np.random.uniform(size=dimension)
 
-    f = get_genz_function(GenzFunctionType.OSCILLATORY, d=d, c=c, w=w)
+    f = get_genz_function(GenzFunctionType.OSCILLATORY, d=dimension, c=c, w=w)
 
-    data = np.random.uniform(0, 1, (n_samples, d))
+    data = np.random.uniform(0, 1, (n_samples, dimension))
 
-    model = approximate_by_polynomial_with_least_squares(f, d, None, data)
+    # model = approximate_by_polynomial_with_least_squares(f, dimension, None, data)
+    # print(model.coef_)
 
-    test_data = np.random.uniform(low=0, high=1, size=(n_test_samples, d))
+    coef = approximate_by_polynomial_with_least_squares(f, dimension, None, data)
+
+    test_data = np.random.uniform(low=0, high=1, size=(n_test_samples, dimension))
 
     poly = PolynomialFeatures(degree=n_samples - 1, include_bias=False)
 
     test_data_poly = poly.fit_transform(test_data)
 
-    y_hat = model.predict(test_data_poly)
+    # y_hat = model.predict(test_data_poly)
     y = f(test_data)
+    y_hat = test_data_poly@coef
     print(y_hat)
     print(y)
+
+    err = np.mean(np.square(y-y_hat))
+    print(f'Error: {err}')
+    # print(y_hat)
+    # print(y)
