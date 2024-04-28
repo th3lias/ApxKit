9ffba65b8cd8deca791870a193443aa64a26bbe3
th3lias <emindlberger@gmail.com>
2024-04-28 15:50:52 +0200

    add some sampling strategies plus tests :sparkles:

--- grid/grid_provider.py
+++ grid/grid_provider.py
@@ -18,33 +18,56 @@ class GridProvider:
                  dimension: np.int8,
                  upper_bound: np.float16,
                  lower_bound: np.float16,
-                 q: np.int8,
                  seed: np.int8 = None):
         self.dim = dimension
-        self.q = q
         self.upper_bound = upper_bound
         self.lower_bound = lower_bound
-        self.seed = seed
-        if self.q < self.dim: raise ValueError(f"fineness must at least exceed dimension")
 
         self.mid_point = (self.upper_bound - self.lower_bound) / 2
         self.avg = (self.upper_bound + self.lower_bound) / 2
 
-    def generate(self, grid_type: GridType) -> np.ndarray:
+        if isinstance(seed, np.int8) or isinstance(seed, int):
+            self.seed = seed
+            self.rng = np.random.default_rng(seed=seed)
+
+    def set_seed(self, seed: np.int8):
+        self.seed = seed
+        self.rng = np.random.default_rng(seed=seed)
+
+    def generate(self, grid_type: GridType, num_points: np.int64 = None) -> np.ndarray:
         """
         Generate a grid of given type.
-        :param grid_type: GridType specification, e.g. Chebyshev, Random or Equidistant
+        :param num_points:  Number of points (per dimension!) when generating the grid equidistantly or randomly.
+                            If GridType == EQUIDISTANT we sample uniformly per dimension, i.e. we have num_points**dim
+                            points. If GridType == RANDOM we aim to have the same number of points as in the regular
+                            grid and therefore sample the same num_points**dim number of points uniformly.
+        :param grid_type: GridType specification, e.g. Chebyshev, Random or Equidistant.
         :return: np.ndarray representing the grid
         """
         if not isinstance(grid_type, GridType):
             raise ValueError(f"grid type not supported: {grid_type}")
-        return self._generate_chebyshev_grid()
+        if grid_type == GridType.CHEBYSHEV:
+            return self._generate_chebyshev_grid()
+        else:
+            if num_points is None:
+                raise ValueError(f"Please provide how many points to generate subspace.")
+            if grid_type == GridType.REGULAR:
+                return self._generate_equidistant_grid(num_points=num_points)
+            if grid_type == GridType.RANDOM:
+                return self._generate_random_grid(num_points=num_points)
 
-    def _generate_random_grid(self):
-        pass
+    def _generate_random_grid(self, num_points: np.int64) -> np.ndarray:
+        return self.rng.uniform(low=self.lower_bound, high=self.upper_bound, size=(num_points ** self.dim, self.dim))
 
-    def _generate_equidistant_grid(self):
-        pass
+    def _generate_equidistant_grid(self, num_points: np.int64) -> np.ndarray:
+        lower = [self.lower_bound] * self.dim
+        upper = [self.upper_bound] * self.dim
+        num_points = [num_points] * self.dim
+
+        axes = [np.linspace(lower[i], upper[i], num_points[i]) for i in range(self.dim)]
+
+        mesh = np.meshgrid(*axes, indexing='ij')
+        return np.stack(mesh, axis=-1).reshape(-1, self.dim)
 
     def _generate_chebyshev_grid(self):
         m = self._generate_m()
--- tests/grid_provider.py
+++ tests/grid_provider.py
@@ -1,27 +1,47 @@
 import unittest
 import numpy as np
-from grid import grid_provider
+from grid.grid_provider import GridProvider
 from grid.grid_type import GridType
+from utils import utils
 
 
 class TestGridProvider(unittest.TestCase):
-    def test_q_d_error(self):
-        with self.assertRaises(ValueError):
-            provider = grid_provider.GridProvider(np.int8(4), np.zeros(1), np.zeros(1), np.int8(1))
 
     def test_provider_type_error(self):
-        provider = grid_provider.GridProvider(np.int8(4), np.zeros(1), np.zeros(1), np.int8(4))
+        provider = GridProvider(np.int8(4), np.float16(1), np.float16(0))
         with self.assertRaises(ValueError):
-            grid = provider.generate(grid_type=1)
+            grid = provider.generate(grid_type='1')
 
-    def test_provider_create_chebyshev_x_i(self):
-        provider = grid_provider.GridProvider(np.int8(4), np.zeros(1), np.zeros(1), np.int8(4))
-        provider.generate(grid_type=GridType.CHEBYSHEV)
-        m = provider._generate_m()
-        self.assertIsInstance(m, np.ndarray)
-        np.testing.assert_array_equal(m, np.array([1, 3, 5, 9], dtype=np.int8))
-        # TODO: Expand this test case.
 
+class VisualTests(unittest.TestCase):
+
+    @staticmethod
+    def test_equidistant_provider_2d():
+        provider = GridProvider(np.int8(2), np.float16(1), np.float16(0))
+        grid = provider.generate(grid_type=GridType.REGULAR, num_points=16)
+        print(f"Grid dimensions: {grid.shape}")
+        utils.visualize_point_grid_2d(grid, alpha=np.int8(1))
+
+    @staticmethod
+    def test_random_provider_3d():
+        provider = GridProvider(np.int8(3), np.float16(1), np.float16(0), seed=np.int8(42))
+        grid = provider.generate(grid_type=GridType.RANDOM, num_points=16)
+        print(f"Grid dimensions: {grid.shape}")
+        utils.visualize_point_grid_3d(grid, alpha=np.int8(1))
+
+    @staticmethod
+    def test_equidistant_provider_3d():
+        provider = GridProvider(np.int8(3), np.float16(1), np.float16(0))
+        grid = provider.generate(grid_type=GridType.REGULAR, num_points=16)
+        print(f"Grid dimensions: {grid.shape}")
+        utils.visualize_point_grid_3d(grid, alpha=np.int8(1))
+
+    @staticmethod
+    def test_random_provider_2d():
+        provider = GridProvider(np.int8(2), np.float16(1), np.float16(0), seed=np.int8(42))
+        grid = provider.generate(grid_type=GridType.RANDOM, num_points=16)
+        print(f"Grid dimensions: {grid.shape}")
+        utils.visualize_point_grid_2d(grid, alpha=np.int8(1))
 
 if __name__ == '__main__':
     unittest.main()
