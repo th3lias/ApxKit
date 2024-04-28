a5b7e9838672dcb619d52a0040f96b174675a116
Jakob Eggl <jakob.eggl@gmail.com>
2024-04-28 14:00:28 +0200

    Fixed the Genz Functions and the Unittests :bug:

--- genz/genz_functions.py
+++ genz/genz_functions.py
@@ -19,39 +19,92 @@ def get_genz_function(function_type: GenzFunctionType, c: np.array, w: np.array,
         raise ValueError("Wrong input type for function_type. Use a GenzFunctionType")
 
     if function_type == GenzFunctionType.OSCILLATORY:
-        return lambda x: np.cos(np.inner(c, x) + 2 * np.pi * w[0]).squeeze()
+        def f(x):
+
+            if type(x) != np.ndarray:
+                raise ValueError("Cannot work with non-numpy arrays")
+
+            x = x.squeeze()
+            if x.ndim == 1:
+                if d != 1:
+                    return np.cos(np.inner(c, x) + 2 * np.pi * w[0]).squeeze()
+                else:
+                    return np.array([np.cos(i * c[0] + 2 * np.pi * w[0]) for i in x])
+            elif x.ndim == 2:
+                return np.cos(np.inner(c, x) + 2 * np.pi * w[0]).squeeze()
+            else:
+                raise ValueError("Cannot handle an array with more than 2 dimensions")
+
+        return f
 
     if function_type == GenzFunctionType.PRODUCT_PEAK:
-        return lambda x: np.prod(1 / (1 / (np.square(c)) + np.square(x - w)), axis=1).squeeze()
+
+        def f(x):
+            if type(x) != np.ndarray:
+                raise ValueError("Cannot work with non-numpy arrays")
+            if x.ndim == 1:
+                return 1 / (1 / (np.square(c)) + np.square(x - w)).squeeze()
+            elif x.ndim == 2:
+                return np.prod(1 / (1 / (np.square(c)) + np.square(x - w)), axis=1).squeeze()
+            else:
+                raise ValueError("Cannot handle an array with more than 2 dimensions")
+
+        return f
 
     if function_type == GenzFunctionType.CORNER_PEAK:
-        return lambda x: 1 / (np.power((1 + np.inner(c, x)), d + 1)).squeeze()
+
+        def f(x):
+            if type(x) != np.ndarray:
+                raise ValueError("Cannot work with non-numpy arrays")
+            if x.ndim == 1:
+                if d != 1:
+                    return 1 / (np.power((1 + np.inner(c, x)), d + 1)).squeeze()
+                else:
+                    return 1 / np.array([np.power(1 + i * c[0], d + 1) for i in x])
+            elif x.ndim == 2:
+                return 1 / (np.power((1 + np.inner(c, x)), d + 1)).squeeze()
+            else:
+                raise ValueError("Cannot handle an array with more than 2 dimensions")
+
+        return f
 
     if function_type == GenzFunctionType.GAUSSIAN:
-        return lambda x: np.exp(-np.sum(np.square(np.multiply(c, x - w)), axis=1)).squeeze()
+        def f(x):
+            if type(x) != np.ndarray:
+                raise ValueError("Cannot work with non-numpy arrays")
+
+            if x.ndim == 1:
+                return np.exp(-np.square(np.multiply(c, x - w))).squeeze()
+            elif x.ndim == 2:
+                return np.exp(-np.sum(np.square(np.multiply(c, x - w)), axis=1)).squeeze()
+            else:
+                raise ValueError("Cannot handle an array with more than 2 dimensions")
+
+        return f
 
     if function_type == GenzFunctionType.CONTINUOUS:
-        return lambda x: np.exp(-np.sum(np.multiply(c, np.abs(x - w)), axis=1)).squeeze()
+        def f(x):
+            if type(x) != np.ndarray:
+                raise ValueError("Cannot work with non-numpy arrays")
+            if x.ndim == 1:
+                return np.exp(-np.multiply(c, np.abs(x - w))).squeeze()
+            elif x.ndim == 2:
+                return np.exp(-np.sum(np.multiply(c, np.abs(x - w)), axis=1)).squeeze()
+            else:
+                raise ValueError("Cannot handle an array with more than 2 dimensions")
+
+        return f
 
     if function_type == GenzFunctionType.DISCONTINUOUS:
         if d == 1:
             def f(x):
                 x = x.squeeze()
-                mask = x > w[0]
-                res = np.exp(c[0]*x)
-                res[mask] = 0
-                return res
+                return np.array([0 if i > w[0] else np.exp(c[0] * i) for i in x])
+
         elif d > 1:
             def f(x):
                 x = x.squeeze()
-                mask1 = x[:,0] > w[0]
-                mask2 = x[:,1] > w[1]
-                mask = np.empty_like(mask1)
-                np.bitwise_or(mask1, mask2, out=mask)
-                res = np.exp(np.inner(x, c)).squeeze()
-                res[mask] = 0
-                return res
+                return np.array([0 if i[0] > w[0] or i[1] > w[1] else np.exp(np.inner(i, c)) for i in x])
         else:
             raise ValueError("Wrong dimension!")
         return f
-
--- tests/genz_functions.py
+++ tests/genz_functions.py
@@ -3,258 +3,389 @@ import numpy as np
 from genz.genz_functions import get_genz_function
 from genz.genz_function_types import GenzFunctionType
 
+
 class GenzFunctionTests(unittest.TestCase):
 
     @staticmethod
     def get_1d_testpoints():
-
-        test_points = [0, 1, 1 / 3, -1, -0.25, 1]
+        test_points = np.array([0, 1, 0.35, -1 / 6, -0.25, 1])
 
         return test_points
 
     @staticmethod
+    def get_1d_test_points_2():
+        return np.array([1, 0, -2, 1.5, np.sqrt(2)])
+
+    @staticmethod
     def get_2d_testpoints():
-        test_points = [np.array([0, 0]), np.array([4, 10]), np.array([1, 1]), np.array([-1, 1]), np.array([2, 0])]
+        test_points = np.array([[0, 0], [4, 10], [1, 1], [-1, 1], [2, 0]])
 
         return test_points
+
+    @staticmethod
+    def get_4d_testpoints():
+        sample_1 = [1.5, 2.3, -1.4, 0]
+        sample_2 = [-1.5, 1, -0.11, 2.123]
+        sample_3 = [np.sqrt(2), np.exp(1), -np.sin(2), np.sqrt(3)]
+
+        return np.array([sample_1, sample_2, sample_3])
 
     def test_osciallatory_1d_1(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.OSCILLATORY, np.array([1]), np.array([0]), d)
-        f = lambda t: np.cos(t)
+        f = lambda t: np.cos(t).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
+
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
 
     def test_osciallatory_1d_2(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.OSCILLATORY, np.array([0.5]), np.array([-0.25]), d)
-        f = lambda t: np.sin(0.5 * t)
+        f = lambda t: np.sin(0.5 * t).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
 
-    def test_osciallatory_2d_1(self):
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
+
+    def test_osciallatory_2d(self):
         d = 2
 
         f_hat = get_genz_function(GenzFunctionType.OSCILLATORY, np.array([-1, 2]), np.array([-2.1, 0]), d)
-        f = lambda t: np.cos(2 * np.pi * -2.1 - t[0] + 2 * t[1])
+        f = lambda t: np.cos(-2 * np.pi * 2.1 - t[:, 0] + 2 * t[:, 1])
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data = self.get_2d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
 
-    def test_osciallatory_2d_2(self):
-        d = 2
+        self.assertTrue(np.isclose(y_hat, y).all())
 
-        f_hat = get_genz_function(GenzFunctionType.OSCILLATORY, np.array([-12, -12]), np.array([0, 0]), d)
-        f = lambda t: np.cos(- 12 * t[0] - 12 * t[1])
+    def test_osciallatory_4d(self):
+        d = 4
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        f_hat = get_genz_function(GenzFunctionType.OSCILLATORY,
+                                  np.array([-12, -12, 1, 2.3]), np.array([1.2, 0, 1, 4]), d)
+        f = lambda t: np.cos(- 12 * t[:, 0] - 12 * t[:, 1] + t[:, 2] + 2.3 * t[:, 3] + 2 * np.pi * 1.2)
+
+        data = self.get_4d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
+
+        self.assertTrue(np.isclose(y_hat, y).all())
 
     def test_productpeak_1d_1(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.PRODUCT_PEAK, c=np.array([np.sqrt(2)]), w=np.array([3 / 8]), d=d)
-        f = lambda t: 1 / (np.power(np.sqrt(2), -2) + np.square(t - 3 / 8))
+        f = lambda t: 1 / (np.power(np.sqrt(2), -2) + np.square(t - 3 / 8)).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
+
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
 
     def test_productpeak_1d_2(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.PRODUCT_PEAK, c=np.array([3]), w=np.array([1]), d=d)
-        f = lambda t: 1 / (1 / (np.square(3)) + np.square((t - 1)))
+        f = lambda t: 1 / (1 / (np.square(3)) + np.square((t - 1))).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
 
-    def test_productpeak_2d_1(self):
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
+
+    def test_productpeak_2d(self):
         d = 2
 
         f_hat = get_genz_function(GenzFunctionType.PRODUCT_PEAK, c=np.array([0.2, 1.5]), w=np.array([0.5, 0.25]), d=d)
-        f = lambda t: 1 / ((np.power(0.2, -2) + np.square(t[0] - 0.5)) * (np.power(1.5, -2) + np.square(t[1] - 0.25)))
+        f = lambda t: 1 / (
+                (np.power(0.2, -2) + np.square(t[:, 0] - 0.5)) * (np.power(1.5, -2) + np.square(t[:, 1] - 0.25)))
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data = self.get_2d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
 
-    def test_productpeak_2d_2(self):
-        d = 2
+        self.assertTrue(np.isclose(y_hat, y).all())
 
-        f_hat = get_genz_function(GenzFunctionType.PRODUCT_PEAK, c=np.array([-0.7, 0.2]), w=np.array([2.1, -1.0]), d=d)
-        f = lambda t: 1 / ((np.power(-0.7, -2) + np.square(t[0] - 2.1)) * (np.power(0.2, -2) + np.square(t[1] + 1)))
+    def test_productpeak_4d(self):
+        d = 4
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        f_hat = get_genz_function(GenzFunctionType.PRODUCT_PEAK, c=np.array([-0.7, 0.2, 0.1, 1 / 3]),
+                                  w=np.array([2.1, -1.0, 0, 0.5]), d=d)
+        f = lambda t: 1 / ((100 / 49 + np.square(t[:, 0] - 2.1)) * (25 + np.square(t[:, 1] + 1)) * (
+                100 + np.square(t[:, 2])) * (9 + np.square(t[:, 3] - 0.5)))
+
+        data = self.get_4d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
+
+        self.assertTrue(np.isclose(y_hat, y).all())
 
     def test_cornerpeak_1d_1(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.CORNER_PEAK, c=np.array([np.exp(1)]), w=None, d=d)
-        f = lambda t: 1 / (np.power((1 + np.exp(1) * t), d + 1))
+        f = lambda t: 1 / (np.power((1 + np.exp(1) * t), d + 1)).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
+
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
 
     def test_cornerpeak_1d_2(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.CORNER_PEAK, c=np.array([1]), w=None, d=d)
-        f = lambda t: 1 / (np.power((1 + t), d + 1))
+        f = lambda t: 1 / (np.power((1 + t), d + 1)).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
 
-    def test_cornerpeak_2d_1(self):
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
+
+    def test_cornerpeak_2d(self):
         d = 2
 
         f_hat = get_genz_function(GenzFunctionType.CORNER_PEAK, c=np.array([0.2, 1.5]), w=None, d=d)
-        f = lambda t: np.power((1 + 0.2 * t[0] + 1.5 * t[1]), -d - 1)
+        f = lambda t: np.power((1 + 0.2 * t[:, 0] + 1.5 * t[:, 1]), -d - 1)
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data = self.get_2d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
 
-    def test_cornerpeak_2d_2(self):
-        d = 2
+        self.assertTrue(np.isclose(y_hat, y).all())
 
-        f_hat = get_genz_function(GenzFunctionType.CORNER_PEAK, c=np.array([-0.7, 0.2]), w=None, d=d)
-        f = lambda t: np.power((1 + -0.7 * t[0] + 0.2 * t[1]), -d - 1)
+    def test_cornerpeak_4d(self):
+        d = 4
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        f_hat = get_genz_function(GenzFunctionType.CORNER_PEAK, c=np.array([-0.7, 0.2, 0.1, 1.2]), w=None, d=d)
+        f = lambda t: np.power((1 + -0.7 * t[:, 0] + 0.2 * t[:, 1]) + 0.1 * t[:, 2] + 1.2 * t[:, 3], -d - 1)
+
+        data = self.get_4d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
+
+        self.assertTrue(np.isclose(y_hat, y).all())
 
     def test_gaussian_1d_1(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.GAUSSIAN, c=np.array([0.6]), w=np.array([0.35]), d=d)
-        f = lambda t: np.exp(-0.36 * (t - 0.35) ** 2)
+        f = lambda t: np.exp(-0.36 * np.square(t - 0.35)).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
+
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
 
     def test_gaussian_1d_2(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.GAUSSIAN, c=np.array([-0.1]), w=np.array([-0.1]), d=d)
-        f = lambda t: np.exp(-0.01 * (t + 0.1) ** 2)
+        f = lambda t: np.exp(-0.01 * (t + 0.1) ** 2).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
 
-    def test_gaussian_2d_1(self):
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
+
+    def test_gaussian_2d(self):
         d = 2
 
         f_hat = get_genz_function(GenzFunctionType.GAUSSIAN, c=np.array([0.2, 1.5]), w=np.array([1, 2]), d=d)
-        f = lambda t: np.exp(-0.04 * (t[0] - 1) ** 2 - 2.25 * (t[1] - 2) ** 2)
+        f = lambda t: np.exp(-0.04 * (t[:, 0] - 1) ** 2 - 2.25 * (t[:, 1] - 2) ** 2)
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data = self.get_2d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
 
-    def test_gaussian_2d_2(self):
-        d = 2
+        self.assertTrue(np.isclose(y_hat, y).all())
 
-        f_hat = get_genz_function(GenzFunctionType.GAUSSIAN, c=np.array([np.sqrt(2), 1 / 6]), w=np.array([0, -2]), d=d)
-        f = lambda t: np.exp(-2 * (t[0]) ** 2 - 1 / 36 * (t[1] + 2) ** 2)
+    def test_gaussian_4d(self):
+        d = 4
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        f_hat = get_genz_function(GenzFunctionType.GAUSSIAN, c=np.array([np.sqrt(2), 1 / 6, 0, 1]),
+                                  w=np.array([0, -2, 0.17, -1.42]), d=d)
+        f = lambda t: np.exp(-2 * (t[:, 0]) ** 2 - 1 / 36 * (t[:, 1] + 2) ** 2 - (t[:, 3] + 1.42) ** 2)
+
+        data = self.get_4d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
+
+        self.assertTrue(np.isclose(y_hat, y).all())
 
     def test_continuous_1d_1(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.CONTINUOUS, c=np.array([0.6]), w=np.array([0.35]), d=d)
-        f = lambda t: np.exp(-0.6 * np.abs(t - 0.35))
+        f = lambda t: np.exp(-0.6 * np.abs(t - 0.35)).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
+
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
 
     def test_continuous_1d_2(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.CONTINUOUS, c=np.array([-0.1]), w=np.array([-0.1]), d=d)
-        f = lambda t: np.exp(0.1 * np.abs(t + 0.1))
+        f = lambda t: np.exp(0.1 * np.abs(t + 0.1)).squeeze()
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
 
-    def test_continuous_2d_1(self):
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
+
+    def test_continuous_2d(self):
         d = 2
 
         f_hat = get_genz_function(GenzFunctionType.CONTINUOUS, c=np.array([0.2, 1.5]), w=np.array([1, 2]), d=d)
-        f = lambda t: np.exp(-0.2 * np.abs(t[0] - 1) - 1.5 * np.abs(t[1] - 2))
+        f = lambda t: np.exp(-0.2 * np.abs(t[:, 0] - 1) - 1.5 * np.abs(t[:, 1] - 2))
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data = self.get_2d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
 
-    def test_continuous_2d_2(self):
-        d = 2
+        self.assertTrue(np.isclose(y_hat, y).all())
 
-        f_hat = get_genz_function(GenzFunctionType.CONTINUOUS, c=np.array([np.sqrt(2), 1 / 6]), w=np.array([0, -2]),
+    def test_continuous_4d(self):
+        d = 4
+
+        f_hat = get_genz_function(GenzFunctionType.CONTINUOUS, c=np.array([np.sqrt(2), 1 / 6, 0.237, -3]),
+                                  w=np.array([0, -2, 1, -1 / 2]),
                                   d=d)
-        f = lambda t: np.exp(-np.sqrt(2) * np.abs(t[0]) - 1 / 6 * np.abs(t[1] + 2))
+        f = lambda t: np.exp(
+            -np.sqrt(2) * np.abs(t[:, 0]) - 1 / 6 * np.abs(t[:, 1] + 2) - 0.237 * np.abs(t[:, 2] - 1) + 3 * np.abs(
+                t[:, 3] + 1 / 2))
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data = self.get_4d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
+
+        self.assertTrue(np.isclose(y_hat, y).all())
 
     def test_discontinuous_1d_1(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.DISCONTINUOUS, c=np.array([0.6]), w=np.array([0.35]), d=d)
 
-        def f(t):
-            if t > 0.35:
-                return 0
-            else:
-                return np.exp(0.6 * t)
+        def f(x):
+            return np.array([0 if i > 0.35 else np.exp(0.6 * i) for i in x])
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(np.array([x])), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
+
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
 
     def test_discontinuous_1d_2(self):
         d = 1
 
         f_hat = get_genz_function(GenzFunctionType.DISCONTINUOUS, c=np.array([-0.1]), w=np.array([-0.1]), d=d)
 
-        def f(t):
-            if t > -0.1:
-                return 0
-            else:
-                return np.exp(-0.1 * t)
+        def f(x):
+            return np.array([0 if i > -0.1 else np.exp(-0.1 * i) for i in x])
 
-        for x in self.get_1d_testpoints():
-            self.assertAlmostEqual(f_hat(np.array([x])), f(x), msg=f"x={x}")
+        data_1 = self.get_1d_testpoints()
+        data_2 = self.get_1d_test_points_2()
+        y_hat_1 = f_hat(data_1)
+        y_1 = f(data_1)
+        y_hat_2 = f_hat(data_2)
+        y_2 = f(data_2)
 
-    def test_discontinuous_2d_1(self):
+        self.assertTrue(np.isclose(y_hat_1, y_1).all())
+        self.assertTrue(np.isclose(y_hat_2, y_2).all())
+
+    def test_discontinuous_2d(self):
         d = 2
 
         f_hat = get_genz_function(GenzFunctionType.DISCONTINUOUS, c=np.array([0.2, 1.5]), w=np.array([1, 2]), d=d)
 
-        def f(t):
-            if t[0] > 1 or t[1] > 2:
-                return 0
-            else:
-                return np.exp(0.2 * t[0] + 1.5 * t[1])
+        def f(x):
+            return np.array([0 if i[0] > 1 or i[1] > 2 else np.exp(0.2 * i[0] + 1.5 * i[1]) for i in x])
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data = self.get_2d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
 
-    def test_discontinuous_2d_2(self):
-        d = 2
+        self.assertTrue(np.isclose(y_hat, y).all())
 
-        f_hat = get_genz_function(GenzFunctionType.DISCONTINUOUS, c=np.array([np.sqrt(2), 1 / 6]), w=np.array([0, -2]),
+    def test_discontinuous_4d(self):
+        d = 4
+
+        f_hat = get_genz_function(GenzFunctionType.DISCONTINUOUS, c=np.array([np.sqrt(2), 1 / 6, 1.1, -0.7]),
+                                  w=np.array([0, -2, 0, 0]),
                                   d=d)
 
-        def f(t):
-            if t[0] > 0 or t[1] > -2:
-                return 0
-            else:
-                return np.exp(np.sqrt(2) * t[0] + 1 / 6 * t[1])
+        def f(x):
+            return np.array(
+                [0 if i[0] > 0 or i[1] > -2 else np.exp(np.sqrt(2) * i[0] + 1 / 6 * i[1] + 1.1 * i[2] - 0.7 * i[3]) for
+                 i in x])
 
-        for x in self.get_2d_testpoints():
-            self.assertAlmostEqual(f_hat(x), f(x), msg=f"x={x}")
+        data = self.get_4d_testpoints()
+        y_hat = f_hat(data)
+        y = f(data)
+
+        self.assertTrue(np.isclose(y_hat, y).all())
 
 
 if __name__ == '__main__':
