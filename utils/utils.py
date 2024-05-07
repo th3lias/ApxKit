e7a2f0610527af697968b40d4a835e02c35b7a56
Jakob Eggl <jakob.eggl@gmail.com>
2024-05-07 13:03:15 +0200

    Small changes s.t. we directly hand over the grid where it should be evaluated in order to ensure that it is the same everytime :bug:

--- utils/utils.py
+++ utils/utils.py
@@ -4,49 +4,36 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def ell_2_error_estimate(f: Callable, f_hat: Callable, d: np.int8, no_samples: np.int16,
-                         lower_bound: np.float64 = np.float64(-1.0),
-                         upper_bound: np.float64 = np.float64(1.0)) -> np.float64:
+def ell_2_error_estimate(f: Callable, f_hat: Callable, grid:np.ndarray) -> np.float64:
     """
-    Calculates the ell_2 error estimate by sampling no_samples points in [lower_bound, upper_bound]^d and
+    Calculates the ell_2 error estimate by comparing the true function and the approximation f_hat on a test grid by
     calculating the mean-squared absolute difference
     :param f: function that should be approximated
     :param f_hat: approximation of the function
-    :param d: dimension of the function
-    :param no_samples: number of samples
-    :param lower_bound: lower bound of the rectangle to sample from
-    :param upper_bound: upper bound of the rectangle to sample from
+    :param grid: grid where the approximation should be compared vs the original function
     :return: error estimate
     """
-    points = np.random.uniform(low=lower_bound, high=upper_bound, size=(no_samples, d))
 
-    y_hat = f_hat(points)
-    y = f(points)
+    y_hat = f_hat(grid)
+    y = f(grid)
 
     error = np.sqrt(np.mean(np.square(np.abs(y - y_hat)))).squeeze()
 
     return error
 
 
-def max_abs_error(f: Callable, f_hat: Callable, d: np.int8, no_samples: np.int16,
-                  lower_bound: np.float64 = np.float64(-1.0),
-                  upper_bound: np.float64 = np.float64(1.0)) -> np.float64:
+def max_abs_error(f: Callable, f_hat: Callable, grid:np.ndarray) -> np.float64:
     """
-        Calculates the estimated max absolute value distance by sampling no_samples points in
-        [lower_bound, upper_bound]^d and calculating the max absolute value difference at those points
+        Calculates the estimated max absolute value distance by comparing the true function and the approximation f_hat
+        on a test grid by calculating the mean-squared absolute difference
         :param f: function that should be approximated
         :param f_hat: approximation of the function
-        :param d: dimension of the function
-        :param no_samples: number of samples
-        :param lower_bound: lower bound of the rectangle to sample from
-        :param upper_bound: upper bound of the rectangle to sample from
+        :param grid: grid where the approximation should be compared vs the original function
         :return: error estimate
         """
 
-    points = np.random.uniform(low=lower_bound, high=upper_bound, size=(no_samples, d))
-    y_hat = f_hat(points)
-    y = f(points)
+    y_hat = f_hat(grid)
+    y = f(grid)
 
     error = np.max(np.abs(y_hat - y)).squeeze()
 
