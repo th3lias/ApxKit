4496900bdd8c9815e626e5bed34e91fb917c06fa
Jakob Eggl <jakob.eggl@gmail.com>
2024-05-12 19:27:12 +0200

    Corrected a small bug/typo :bug:

--- utils/utils.py
+++ utils/utils.py
@@ -17,7 +17,7 @@ def l2_error_function_values(y: np.ndarray, y_hat: np.ndarray) -> Union[np.float
     if y_hat.ndim == 1:
         error = np.sqrt(np.mean(np.square(np.abs(y - y_hat)))).squeeze()
     else:
-        error = np.sqrt(np.mean(np.square(np.abs(y - y_hat))), axis=1).squeeze()
+        error = np.sqrt(np.mean(np.square(np.abs(y - y_hat)), axis=1)).squeeze()
 
     return error
 
