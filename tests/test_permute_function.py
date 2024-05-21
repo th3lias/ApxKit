40ba30395bc4fb6dfc0fdfd5496bdfc6e269e86c
Jakob Eggl <jakob.eggl@gmail.com>
2024-05-21 08:07:30 +0200

    Improved the tests for the permute function :sparkles:

--- tests/test_permute_function.py
+++ tests/test_permute_function.py
@@ -18,7 +18,7 @@ class PermutationTests(unittest.TestCase):
         next(gen)
         next(gen)
 
-        self.assertTrue(np.all(next(gen) == np.array([0,1,2,3,4,5,6,9,7,8])))
+        self.assertTrue(np.all(next(gen) == np.array([0, 1, 2, 3, 4, 5, 6, 9, 7, 8])))
 
     def test_length_size10(self):
         size = 10
@@ -42,13 +42,59 @@ class PermutationTests(unittest.TestCase):
         next(gen)
         next(gen)
 
-        self.assertTrue(np.all(next(gen) == np.array([2,0,1])))
+        self.assertTrue(np.all(next(gen) == np.array([2, 0, 1])))
 
     def test_length_size3(self):
         size = 3
         array = np.arange(size)
 
         gen = SmolyakInterpolator._permute(array)
+
+        s = sum(1 for _ in gen)
+
+        self.assertEqual(s, math.factorial(size))
+
+    def test_array_drop_duplicate(self):
+        array = np.array([1, 3, 4, 2, 2, 2])
+
+        gen = SmolyakInterpolator._permute(array, drop_duplicates=True)
+
+        self.assertTrue(np.all(next(gen) == np.sort(array)))
+
+        for i in range(110):
+            _ = next(gen)
+
+        self.assertTrue(np.all(next(gen) == np.array([4,2,2,3,1,2])))
+
+    def test_length_drop_duplicate(self):
+        array = np.array([1, 3, 4, 2, 2, 2])
+
+        size = array.shape[0]
+
+        gen = SmolyakInterpolator._permute(array, drop_duplicates=True)
+
+        s = sum(1 for _ in gen)
+
+        self.assertEqual(s, math.factorial(size) / math.factorial(3))
+
+    def test_array_keep_duplicate(self):
+        array = np.array([1, 3, 4, 2, 2, 2])
+
+        gen = SmolyakInterpolator._permute(array, drop_duplicates=False)
+
+        self.assertTrue(np.all(next(gen) == np.sort(array)))
+
+        for i in range(110):
+            _ = next(gen)
+
+        self.assertTrue(np.all(next(gen) == np.array([1,4,2,2,3,2])))
+
+    def test_length_keep_duplicate(self):
+        array = np.array([1, 3, 4, 2, 2, 2])
+
+        size = array.shape[0]
+
+        gen = SmolyakInterpolator._permute(array, drop_duplicates=False)
 
         s = sum(1 for _ in gen)
 
