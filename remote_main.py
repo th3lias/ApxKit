73bb346257d5fb8bf58087a958d4b8221f6355db
Jakob Eggl <jakob.eggl@gmail.com>
2024-07-08 23:17:21 +0200

    Added code to print the Process ID of the remote process :sparkles:

--- remote_main.py
+++ remote_main.py
@@ -5,10 +5,10 @@ import datetime
 if __name__ == '__main__':
     cur_datetime = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
 
-    filename = "console_out.txt"
+    filename_res = os.path.join("results", cur_datetime, "console_out.txt")
+    filename_pid = os.path.join("results", cur_datetime, "pid.txt")
 
-    filename = os.path.join("results", cur_datetime, filename)
+    os.makedirs(os.path.dirname(filename_res), exist_ok=True)
 
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-    os.system(f"nohup {sys.executable} main.py --folder_name {cur_datetime} > {filename} 2>&1 &")
+    os.system(
+        f"nohup {sys.executable} main.py --folder_name {cur_datetime} > {filename_res} 2>&1 & echo $! > {filename_pid}")
