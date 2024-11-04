82024e04b107d6eceba167100af9d886d0ce259a
th3lias <emindlberger@gmail.com>
2024-11-04 17:29:26 +0100

    some investigation :poop:

--- experiments/smolyak_experiments.py
+++ experiments/smolyak_experiments.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from fit import SmolyakFitter
 from function import SmolyakModel, Function, FunctionType, ParametrizedFunctionProvider
+from function.parametrized_f import ParametrizedFunction
 from grid import RuleGridProvider
 
 
@@ -21,6 +23,8 @@ class SmolyakExperimentExecutor:
 			"function": [],
 			"dim": [],
 			"scale": [],
+			"c": [],
+			"w": [],
 			"l_2": [],
 			"l_inf": []
 		}
@@ -31,19 +35,20 @@ class SmolyakExperimentExecutor:
 		for dim in self.dim_list:
 			fitter = SmolyakFitter(dim)
 			rule_grid_provider = RuleGridProvider(dim)
-			test_points = np.random.uniform(0, 1, (self.num_test_points, dim))
+			test_points = np.random.uniform(0, 1, size=(self.num_test_points, dim)).astype(np.float64)
 			self.execute_single_dim_experiment(dim, function_types, fitter, rule_grid_provider, test_points)
+		self.plot_csv()
 	
 	def execute_single_dim_experiment(self, dim: int, function_types: list[FunctionType], fitter: SmolyakFitter,
 	                                  rule_grid_provider: RuleGridProvider, test_points: np.ndarray):
-		c = np.random.uniform(0, 1, dim)
+		c = np.random.uniform(0, 1, dim).astype(np.float64)
 		c = c / np.sum(c) * dim
-		w = np.random.uniform(0, 1, dim)
+		w = np.random.uniform(0, 1, dim).astype(np.float64)
 		for function_type in function_types:
 			function = ParametrizedFunctionProvider.get_function(function_type, dim, c=c, w=w)
 			self.execute_single_function_experiment(function, fitter, rule_grid_provider, test_points)
 	
-	def execute_single_function_experiment(self, function: Function, fitter: SmolyakFitter,
+	def execute_single_function_experiment(self, function: ParametrizedFunction, fitter: SmolyakFitter,
 	                                       rule_grid_provider: RuleGridProvider, test_points):
 		for scale in self.scale_list:
 			grid = rule_grid_provider.generate(scale)
@@ -52,17 +57,17 @@ class SmolyakExperimentExecutor:
 			self.save_stats(function, scale, l_2, l_inf)
 	
 	@staticmethod
-	def evaluate_model(function: Function, model: SmolyakModel, test_points: np.ndarray):
+	def evaluate_model(function: Function, model: SmolyakModel, test_points: np.ndarray) -> tuple[float, float]:
 		"""
 			Evaluate the model on the test points and return the L2 and L_inf norms.
 		"""
-		model_values = model(test_points).reshape(-1, 1)
-		true_values = function(test_points).reshape(-1, 1)
-		l_2 = np.sum(np.square(model_values - true_values)) / function.dim
+		model_values = model(test_points).reshape(-1, 1).astype(np.float64)
+		true_values = function(test_points).reshape(-1, 1).astype(np.float64)
+		l_2 = np.sqrt(np.mean(np.square(model_values - true_values)))
 		l_inf = np.max(np.abs(model_values - true_values))
 		return l_2, l_inf
 	
-	def save_stats(self, function: Function, scale: int, l_2: float, l_inf: float):
+	def save_stats(self, function: ParametrizedFunction, scale: int, l_2: float, l_inf: float):
 		"""
 			Keep the CSV up to date with the current results.
 		"""
@@ -70,17 +75,47 @@ class SmolyakExperimentExecutor:
 			"function": [function.name],
 			"dim": [function.dim],
 			"scale": [scale],
+			"c": str(function.c),
+			"w": str(function.w),
 			"l_2": [l_2],
 			"l_inf": [l_inf]
 		}
 		df = pd.DataFrame(data)
 		df.to_csv(self.results_path, mode='a', header=False, index=False)
+	
+	def plot_csv(self):
+		"""
+			Plot the results from the CSV file using plotly.
+			Plot Scale vs Error.
+		"""
+		plt.figure(figsize=(10, 5))
+		df = pd.read_csv(self.results_path)
+		for function in df['function'].unique():
+			df_function = df[df['function'] == function]
+			plt.plot(df_function['scale'], df_function['l_2'], label=function)
+		plt.xlabel("Scale")
+		plt.ylabel("L2 Norm")
+		plt.legend()
+		plt.show()
 
 
 if __name__ == "__main__":  # Example usage
-	dims = [2, 3, 4, 5, 6]
-	scales = [2, 3, 4, 5]
-	num_points = 10000
+	dims = [3, 4, 5]
+	scales = [4, 5, 6]
+	num_points = 1000
 	experiment_executor = SmolyakExperimentExecutor(dims, scales, num_points)
-	functions = [FunctionType.OSCILLATORY, FunctionType.GAUSSIAN, FunctionType.G_FUNCTION]
+	functions = [
+		FunctionType.OSCILLATORY,
+		FunctionType.PRODUCT_PEAK,
+		FunctionType.GAUSSIAN,
+		# FunctionType.CORNER_PEAK,  --- Tasmanian has huge problems with this function
+		FunctionType.CONTINUOUS,
+		FunctionType.DISCONTINUOUS,
+		FunctionType.G_FUNCTION,
+		# FunctionType.MOROKOFF_CALFISCH_1,  --- The calculation of this function always throws a warning
+		FunctionType.MOROKOFF_CALFISCH_2,
+		# FunctionType.ROOS_ARNOLD,  --- Tasmanian has quite some problems with this function
+		FunctionType.BRATLEY,
+		FunctionType.ZHOU
+	]
 	experiment_executor.execute_random_experiments(functions)
--- function/utils.py
+++ function/utils.py
@@ -5,83 +5,42 @@ def oscillatory(x, d, c, w):
     """
         Oscillatory function.
     """
-    if x.ndim == 1:
-        if d != 1:
-            return np.cos(np.inner(c, x) + 2 * np.pi * w[0]).squeeze()
-        else:
-            return np.array([np.cos(i * c[0] + 2 * np.pi * w[0]) for i in x])
-    elif x.ndim == 2:
-        return np.cos(np.inner(c, x) + 2 * np.pi * w[0]).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return np.cos(np.dot(x, c) + 2 * np.pi * w[0])
 
 
 def product_peak(x, d, c, w):
     """
         Product peak function.
     """
-    if not isinstance(x, np.ndarray):
-        raise ValueError("Cannot work with non-numpy arrays")
-    if x.ndim == 1:
-        return 1 / (1 / (np.square(c)) + np.square(x - w)).squeeze()
-    elif x.ndim == 2:
-        return np.prod(1 / (1 / (np.square(c)) + np.square(x - w)), axis=1).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return np.prod(1 / (1/np.square(c)) + np.square(x - w), axis=1)
 
 
 def corner_peak(x, d, c, w):
     """
         Corner peak function.
     """
-    if not isinstance(x, np.ndarray):
-        raise ValueError("Cannot work with non-numpy arrays")
-    if x.ndim == 1:
-        if d != 1:
-            return 1 / (np.power((1 + np.inner(c, x)), d + 1)).squeeze()
-        else:
-            return 1 / np.array([np.power(1 + i * c[0], d + 1) for i in x])
-    elif x.ndim == 2:
-        return 1 / (np.power((1 + np.inner(c, x)), d + 1)).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return np.power(1+np.dot(x, c), -(d+1))
 
 
 def gaussian(x, d, c, w):
     """
         Gaussian function.
     """
-    if not isinstance(x, np.ndarray):
-        raise ValueError("Cannot work with non-numpy arrays")
-    if x.ndim == 1:
-        return np.exp(-np.square(np.multiply(c, x - w))).squeeze()
-    elif x.ndim == 2:
-        return np.exp(-np.sum(np.square(np.multiply(c, x - w)), axis=1)).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return np.exp(-np.sum(np.square(np.multiply(c, x - w)), axis=1))
 
 
 def continuous(x, d, c, w):
     """
         Continuous function.
     """
-    if not isinstance(x, np.ndarray):
-        raise ValueError("Cannot work with non-numpy arrays")
-    if x.ndim == 1:
-        return np.exp(-np.multiply(c, np.abs(x - w))).squeeze()
-    elif x.ndim == 2:
-        return np.exp(-np.sum(np.multiply(c, np.abs(x - w)), axis=1)).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return np.exp(-np.sum(np.multiply(c, np.abs(x - w)), axis=1))
 
 
 def discountinuous_1d(x, d, c, w):
-    x = x.squeeze()
     return np.array([0 if i > w[0] else np.exp(c[0] * i) for i in x])
 
 
 def discountinuous_nd(x, d, c, w):
-    x = x.squeeze()
     return np.array([0 if i[0] > w[0] or i[1] > w[1] else np.exp(np.inner(i, c)) for i in x])
 
 
@@ -89,95 +48,40 @@ def g_function(x, d, c, w):
     """
         G-function.
     """
-    x = x.squeeze()
-    if x.ndim == 1:
-        if d != 1:
-            return np.prod(np.divide((np.abs(4 * x - 2 - w) + c), 1 + c), axis=1).squeeze()
-        else:
-            return np.array([np.divide(np.abs(4 * i - 2 - w[0]) + c[0], (1 + c[0])) for i in x])
-    elif x.ndim == 2:
-        return np.prod(np.divide((np.abs(4 * x - 2 - w) + c), 1 + c), axis=1).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return np.prod(
+        np.divide(
+            np.abs(4 * x - 2 - w) + c,
+            1 + c
+        ), axis=1
+    )
 
 
 def morokoff_calfisch_1(x, d, c, w):
     """
         Morokoff Calfisch function.
     """
-    x = x.squeeze()
-    if x.ndim == 1:
-        if d != 1:
-            return (1 + 1 / d) ** d * (np.prod(np.multiply(x, c) + w, axis=1) ** (1 / d)).squeeze()
-        else:
-            return np.array([2 * (i * c[0] + w[0]) for i in x])
-    elif x.ndim == 2:
-        return (1 + 1 / d) ** d * (np.prod(np.multiply(x, c) + w, axis=1) ** (1 / d)).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return (1+1/d)**d * np.prod(np.multiply(x, c) + w, axis=1)**(1/d)
 
 
 def morokoff_calfisch_2(x, d, c, w):
     """
         Morokoff Calfisch function.
     """
-    x = x.squeeze()
-    if x.ndim == 1:
-        if d != 1:
-            return ((d - 1 / 2) ** (-d) * np.prod(d - np.multiply(c, x) - w, axis=1)).squeeze()
-        else:
-            return np.array([2 * (1 - c[0] * i - w[0]) for i in x])
-    elif x.ndim == 2:
-        return ((d - 1 / 2) ** (-d) * np.prod(d - np.multiply(c, x) - w, axis=1)).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return 1/(d-1/2)**d * np.prod(d - np.multiply(c, x) - w, axis=1)
 
 
 def roos_arnold(x, d, c, w):
     """
         Roos Arnold Function.
     """
-    x = x.squeeze()
-    if x.ndim == 1:
-        if d != 1:
-            return np.prod(np.abs(4 * np.multiply(c, x) - 2 - w), axis=1).squeeze()
-        else:
-            return np.array([np.abs(4 * (i * c[0]) - 2 - w[0]) for i in x])
-    elif x.ndim == 2:
-        return np.prod(np.abs(4 * np.multiply(c, x) - 2 - w), axis=1).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return np.prod(np.abs(4 * np.multiply(c, x) - 2 - w), axis=1)
 
 
 def bratley(x, d, c, w):
     """
         Bratley function.
     """
-    x = x.squeeze()
-    if x.ndim == 1:
-        if d != 1:
-            val = 0
-            for i in range(1, d + 1):
-                prod = 1
-                for j in range(1, i + 1):
-                    prod *= (c[j] * x[j] - w)
-
-                val += (-1) ** i * prod
-            return np.array(val).squeeze()
-        else:
-            return np.array([-i * c[0] + w[0] for i in x])
-    elif x.ndim == 2:
-        if d != 1:
-            val = np.zeros(x.shape[0])
-            for i in range(1, d + 1):
-                prod = np.ones_like(val)
-                for j in range(1, i + 1):
-                    prod *= (c[j - 1] * x[:, j - 1] - w[j - 1])
-
-                val += (-1) ** i * prod
-            return np.array(val).squeeze()
-    else:
-        raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
+    return np.sum(np.power(-1, np.arange(1, d+1))) * np.prod(np.multiply(c, x) - w, axis=1)
 
 
 def zhou(x, d, c, w):
@@ -190,7 +94,6 @@ def zhou(x, d, c, w):
             phi_1 = (2 * np.pi) ** (-d / 2) * np.exp(-np.sum(np.square(np.multiply(c, (x - w))), axis=1) / 2)
             phi_2 = (2 * np.pi) ** (-d / 2) * np.exp(
                 -np.sum(np.square(np.multiply(c, (x - 1 + w))), axis=1) / 2)
-
             return (10 ** d * 0.5 * (phi_1 + phi_2)).squeeze()
         else:
             phi_1 = [(2 * np.pi) ** (-1 / 2) * np.exp(-np.sum(np.square(c[0] * (i - w[0]))) / 2) for i in x]
@@ -199,7 +102,6 @@ def zhou(x, d, c, w):
     elif x.ndim == 2:
         phi_1 = (2 * np.pi) ** (-d / 2) * np.exp(-np.sum(np.square(np.multiply(c, (x - w))), axis=1) / 2)
         phi_2 = (2 * np.pi) ** (-d / 2) * np.exp(-np.sum(np.square(np.multiply(c, (x - 1 + w))), axis=1) / 2)
-
         return (10 ** d * 0.5 * (phi_1 + phi_2)).squeeze()
     else:
         raise ValueError(f"Cannot handle an array with number of dimension ={x.ndim}")
