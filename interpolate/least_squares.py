removed the new weight calculation with the radius :bug:

interpolate/least_squares.py
@@ -139,11 +139,7 @@ class LeastSquaresInterpolator(Interpolator):
                 points = 2 * points - 1
             elif self.grid.lower_bound != -1.0 or self.grid.upper_bound != 1.0:
                 raise ValueError("The Chebyshev rule only supports the range [-1, 1] or [0, 1]")
-            # TODO: Check that.
-            radius = np.linalg.norm(points, axis=1) / np.sqrt(self.dim)
-            assert radius.shape == (self.grid.get_num_points(),), "Radius should be a 1D array"
-            weight = np.sqrt(np.polynomial.chebyshev.chebweight(radius) / np.pi)
-            # weight = np.sqrt(np.prod(np.polynomial.chebyshev.chebweight(points), axis=1) / np.pi)
+            weight = np.sqrt(np.prod(np.polynomial.chebyshev.chebweight(points) / np.pi, axis=1)) # TODO: pi inside or outside of product?
         elif self.grid.rule == RandomGridRule.UNIFORM:
             weight = np.ones(shape=(self.grid.get_num_points()), dtype=np.float64)
         else: