3e4ebcf8fe76726fc18dd9fa7cbd6416fe024aa6
Jakob Eggl <jakob.eggl@gmail.com>
2026-07-04 11:09:54 +0200

    updated shell script a bit

---
+++ run_job_a100.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=ApxKit
+#SBATCH --partition=gpu-a100
+#SBATCH --gres=gpu:1
+
+# timestamp
+CUR_DATE_TIME=$(date +"%d_%m_%Y_%H_%M_%S")
+RESULT_DIR="results/$CUR_DATE_TIME"
+
+# folder
+mkdir -p "$RESULT_DIR"
+
+# load modules
+module load nvidia/cuda/12.8
+module load python/312
+
+# activate venv
+source .venv/bin/activate
+
+# write job_id to a file in the results folder
+echo $SLURM_JOB_ID > "$RESULT_DIR/pid.txt"
+
+# start main.py and redirect stdout and stderr to console_out.txt in the results folder
+python main.py --folder_name "$CUR_DATE_TIME" > "$RESULT_DIR/console_out.txt" 2>&1
